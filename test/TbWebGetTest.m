5873fb822bc07497cac34b060ce9d2af147d72ba
Ben Heasly <benjamin.heasly@gmail.com>
2016-07-12 13:39:38 -0400

    add webget strategy

---
+++ test/TbWebGetTest.m
@@ -0,0 +1,147 @@
+classdef TbWebGetTest < matlab.unittest.TestCase
+    % Test the Toolbox Toolbox against a contrived Githib repository.
+    %
+    % The contrived GitHub repository at
+    % https://github.com/benjamin-heasly/sample-repo.git has an expected
+    % GitHub release named v0.1.
+    %
+    % The Toolbox Toolbox should be able to download regular and zip files
+    % that are part of the release.
+    %
+    % 2016 benjamin.heasly@gmail.com
+    
+    properties
+        zipUrl = 'https://github.com/benjamin-heasly/sample-repo/archive/v0.1.zip';
+        imageUrl = 'https://github.com/benjamin-heasly/sample-repo/releases/download/v0.1/sample-download.jpg';
+        configPath = fullfile(tempdir(), 'toolbox-config.json');
+        toolboxRoot = fullfile(tempdir(), 'toolboxes');
+        originalMatlabPath;
+    end
+    
+    methods (TestMethodSetup)
+        function saveOriginalMatlabState(obj)
+            obj.originalMatlabPath = path();
+            tbResetMatlabPath('withSelf', true, 'withBuiltIn', true);
+        end
+        
+        function cleanUpTempFiles(obj)
+            if 2 == exist(obj.configPath, 'file')
+                delete(obj.configPath);
+            end
+            
+            if 7 == exist(obj.toolboxRoot, 'dir')
+                rmdir(obj.toolboxRoot, 's');
+            end
+        end
+    end
+    
+    methods (TestMethodTeardown)
+        function restoreOriginalMatlabState(obj)
+            path(obj.originalMatlabPath);
+        end
+    end
+    
+    methods (Test)
+        function testZipFile(obj)
+            record = tbToolboxRecord( ...
+                'type', 'webget', ...
+                'name', 'zipFile', ...
+                'url', obj.zipUrl);
+            results = tbDeployToolboxes( ...
+                'config', record, ...
+                'toolboxRoot', obj.toolboxRoot, ...
+                'restorePath', true);
+            obj.assertEqual(results.status, 0);
+            
+            % zip should now be on the path
+            zipFile = which('v0.1.zip');
+            obj.assertEqual(exist(zipFile, 'file'), 2);
+            
+            % zip contents should now be on the path
+            textFile = which('master.txt');
+            obj.assertEqual(exist(textFile, 'file'), 2);
+        end
+        
+        function testImageFile(obj)
+            % download an image file
+            record = tbToolboxRecord( ...
+                'type', 'webget', ...
+                'name', 'imageFile', ...
+                'url', obj.imageUrl);
+            results = tbDeployToolboxes( ...
+                'config', record, ...
+                'toolboxRoot', obj.toolboxRoot, ...
+                'restorePath', true);
+            obj.assertEqual(results.status, 0);
+            
+            % image should now be on the path
+            imageFile = which('sample-download.jpg');
+            obj.assertEqual(exist(imageFile, 'file'), 2);
+            
+            % should be able to load the image
+            imageData = imread(imageFile);
+            obj.assertEqual(size(imageData), [491 736 3]);
+        end
+        
+        function testUpdate(obj)
+            % download an image file
+            record = tbToolboxRecord( ...
+                'type', 'webget', ...
+                'name', 'update', ...
+                'url', obj.imageUrl);
+            results = tbDeployToolboxes( ...
+                'config', record, ...
+                'toolboxRoot', obj.toolboxRoot, ...
+                'restorePath', true);
+            obj.assertEqual(results.status, 0);
+            
+            % image should now be on the path
+            imageFile = which('sample-download.jpg');
+            obj.assertEqual(exist(imageFile, 'file'), 2);
+            
+            % delay to force updated file to have different time stamp
+            pause(1);
+            
+            % update should replace the image with another copy
+            obtainInfo = dir(imageFile);
+            results = tbDeployToolboxes( ...
+                'config', record, ...
+                'toolboxRoot', obj.toolboxRoot, ...
+                'restorePath', true);
+            obj.assertEqual(results.status, 0);
+            updateInfo = dir(imageFile);
+            obj.assertGreaterThan(datenum(updateInfo.date), datenum(obtainInfo.date));
+        end
+        
+        function testNoUpdate(obj)
+            % download an image file
+            record = tbToolboxRecord( ...
+                'type', 'webget', ...
+                'name', 'noUpdate', ...
+                'url', obj.imageUrl, ...
+                'update', 'never');
+            results = tbDeployToolboxes( ...
+                'config', record, ...
+                'toolboxRoot', obj.toolboxRoot, ...
+                'restorePath', true);
+            obj.assertEqual(results.status, 0);
+            
+            % image should now be on the path
+            imageFile = which('sample-download.jpg');
+            obj.assertEqual(exist(imageFile, 'file'), 2);
+            
+            % delay to force updated file to have different time stamp
+            pause(1);
+            
+            % update should have no effect on the image already downloaded
+            obtainInfo = dir(imageFile);
+            results = tbDeployToolboxes( ...
+                'config', record, ...
+                'toolboxRoot', obj.toolboxRoot, ...
+                'restorePath', true);
+            obj.assertEqual(results.status, 0);
+            updateInfo = dir(imageFile);
+            obj.assertEqual(updateInfo.date, obtainInfo.date);
+        end
+    end
+end
---
+++ toolbox-strategies/TbWebGetStrategy.m
@@ -0,0 +1,43 @@
+classdef TbWebGetStrategy < TbToolboxStrategy
+    % Use HTTP GET to obtain a file.  May be a zip file to explode.
+    %   see websave, unzip
+    %
+    % 2016 benjamin.heasly@gmail.com
+    
+    methods
+        
+        function [command, status, message] = obtain(obj, record, toolboxRoot, toolboxPath)
+            
+            try
+                command = 'mkdir';
+                if 7 ~= exist(toolboxPath, 'dir')
+                    mkdir(toolboxPath);
+                end
+                
+                command = 'websave';
+                [~, resourceBase, resourceExt] = fileparts(record.url);
+                fileName = fullfile(toolboxPath, [resourceBase, resourceExt]);
+                fileName = websave(fileName, record.url);
+                
+                if strcmp(resourceExt, '.zip')
+                    command = 'unzip';
+                    unzip(fileName, toolboxPath);
+                end
+                
+            catch err
+                status = -1;
+                message = err.message;
+                return;
+            end
+            
+            % great!
+            status = 0;
+            message = 'download OK';
+        end
+        
+        function [command, status, message] = update(obj, record, toolboxRoot, toolboxPath)
+            % just keep obtaining.  Use record.neverUpdate to prevent this.
+            [command, status, message] = obj.obtain(record, toolboxRoot, toolboxPath);
+        end
+    end
+end
