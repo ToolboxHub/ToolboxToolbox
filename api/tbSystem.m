a1523273f960aa2f959d7236672d379537a769d2
Ben Heasly <benjamin.heasly@gmail.com>
2016-08-30 15:53:48 -0400

    tbSystem can keep some env vars

--- api/tbSystem.m
+++ api/tbSystem.m
@@ -1,17 +1,23 @@
-function [status, result] = tbSystem(command)
+function [status, result] = tbSystem(command, varargin)
 % Run a comand with system() and a clean environment.
 %
 % [status, result] = tbSystem(command) locates the given command on
 % the system PATH, then invokes the command.  Clears the shell environment
 % beforehand, so unusual environment variables set by Matlab can be
-% ignored. 
+% ignored.
+%
+% tbSystem( ... 'keep' keep) specifies a list of existing environment
+% variables to preserve in the clean shell environment.  The default is {},
+% don't preserve any existing variables.
 %
 % 2016 benjamin.heasly@gmail.com
 
 parser = inputParser();
 parser.addRequired('command', @ischar);
-parser.parse(command);
+parser.addParameter('keep', {}, @iscellstr);
+parser.parse(command, varargin{:});
 command = parser.Results.command;
+keep = parser.Results.keep;
 
 if ispc()
     [status, result] = system(command);
@@ -35,5 +41,15 @@ if 0 ~= status
 end
 whichExecutable = strtrim(result);
 
+% keep some of the environment
+nKeep = numel(keep);
+keepVals = cell(1, 2*nKeep);
+for kk = 1:nKeep
+    keepVals(2*kk-1) = keep(kk);
+    keepVals{2*kk} = getenv(keep{kk});
+end
+keepString = sprintf('%s=%s ', keepVals{:});
+
 % run the command with a clean environment
-[status, result] = system(['env -i ' whichExecutable args]);
+fullCommand = ['env -i ' keepString whichExecutable args];
+[status, result] = system(fullCommand);
