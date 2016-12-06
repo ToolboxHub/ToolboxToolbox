2fe390087cc95e82f830f9fdf4784f47b46e49e3
Ben Heasly <benjamin.heasly@gmail.com>
2016-12-06 14:43:51 -0500

    avoid incompatible testsuite function from later Matlabs

--- runTestsAndExit.m
+++ runTestsAndExit.m
@@ -2,9 +2,10 @@ function results = runTestsAndExit(varargin)
 % Invoke from the command line to run tests with diagnostics and exit code.
 
 % a sample command for poc
-%  matlab -nodisplay -r "runAllTests('setupCommand', 'tbUse(''RenderToolbox4'')', 'tests', '/home/ben/toolboxes/RenderToolbox4/Test/Automated');"
+%  matlab -nodisplay -r "runAllTests('setupCommand', 'tbUse(''RenderToolbox4'')', 'testFolder', '/home/ben/toolboxes/RenderToolbox4/Test/Automated');"
 
 import matlab.unittest.TestRunner;
+import matlab.unittest.TestSuite;
 import matlab.unittest.plugins.TAPPlugin;
 import matlab.unittest.plugins.ToFile;
 import matlab.unittest.Verbosity;
@@ -13,11 +14,11 @@ try
     % input errors should be trapped
     parser = inputParser();
     parser.addParameter('setupCommand', '', @ischar);
-    parser.addParameter('tests', pwd(), @ischar);
+    parser.addParameter('testFolder', pwd(), @ischar);
     parser.addParameter('resultsFile', 'testResults.tap', @ischar);
     parser.parse(varargin{:});
     setupCommand = parser.Results.setupCommand;
-    tests = parser.Results.tests;
+    testFolder = parser.Results.testFolder;
     resultsFile = parser.Results.resultsFile;
     
     if ~isempty(setupCommand)
@@ -26,9 +27,7 @@ try
     end
     
     % gather tests in the given folder (or package, classs, etc.)
-    suite = testsuite(tests, ...
-        'IncludeSubfolders', true, ...
-        'IncludeSubpackages', true);
+    suite = TestSuite.fromFolder(testFolder, 'IncludingSubfolders', true);
     runner = TestRunner.withTextOutput('Verbosity', Verbosity.Verbose);
     if ismethod('TAPPlugin', 'producingVersion13')
         % new TAP plugin format and additional diagnostics
