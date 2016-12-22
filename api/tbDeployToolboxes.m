6fbf156ebb6267d6fe94680e0736033016fb133a
Ben Heasly <benjamin.heasly@gmail.com>
2016-12-22 14:16:15 -0500

    don't try to filter nameless records from empty config

--- api/tbDeployToolboxes.m
+++ api/tbDeployToolboxes.m
@@ -104,11 +104,6 @@ if isempty(config) || ~isstruct(config) || ~isfield(config, 'name')
 end
 
 
-%% Ignore records without names -- they're just comments.
-isComment = cellfun(@isempty, {config.name});
-config = config(~isComment);
-
-
 %% Convert registered toolbox names to "include" records.
 if ~isempty(registered)
     nRegistered = numel(registered);
@@ -125,6 +120,12 @@ if ~isempty(registered)
     end
 end
 
+
+%% Ignore records without names -- they're just comments.
+if ~isempty(config)
+    isComment = cellfun(@isempty, {config.name});
+    config = config(~isComment);
+end
 
 %% Single out one toolbox?
 if ~isempty(name)
