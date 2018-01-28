4bb9e680e9e5c76027035bc99c9fc9a1f1f850c1
David Brainard <brainard@psych.upenn.edu>
2018-01-28 09:47:23 -0500

    Add support for multiple os-specific options in URL record.

---
+++ api/tbProcessMultiOSUrl.m
@@ -0,0 +1,43 @@
+function url = tbProcessMultiOSUrl(multiOSUrl)
+% Convert a multi-computer url to a url for the current computer
+%
+% url = tbProcessMultiComputerURL(multiOSUrl)
+%
+% The toolbox toolbox will accept a string array in the form
+% ['OSType1' 'url1' 'OSType2 'url2' ...] where 'OSTypeN'
+% is a string returned by the Matlab computer command.
+%
+% This allows us to fetch, for example, different precompiled sdk's
+% to match the current os type.
+%
+% Takes advantage of string arrays, and only works with R2016b and later.
+
+% Check for os dependent record.  We can only do this if isstring exists,
+% which is true in R2016b and later.  Otherwise, we cannot handle
+% multi-computer URL records.
+if (exist('isstring','builtin'))
+    if (isstring(multiOSUrl))
+        if (rem(length(multiOSUrl),2) ~= 0)
+            error('Multi-OS URL record must have an even number of entries');
+        end
+        gotURL = false;
+        for ii = 1:2:length(multiOSUrl)
+            if (strcmp(computer,multiOSUrl(ii)))
+                url = char(multiOSUrl(ii+1));
+                gotURL = true;
+                break;
+            end
+        end
+        if (~gotURL)
+            error('Did not find a URL for this OS type in multi-OS URL');
+        end
+    else
+        url = multiOSUrl;
+    end
+else
+    if (~ischar(multiOSUrl))
+        error('Can only handle multi-OS URLs in R2016b and later');
+    end
+    url = multiOSUrl;
+end
+
